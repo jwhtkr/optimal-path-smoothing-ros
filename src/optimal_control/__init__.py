739fab16176c1fc871b10922aa1b0827b7d23d45
Justin Whitaker <justinwhtkr@gmail.com>
2020-05-21 18:17:30 -0600

    Created initial package structure

---
+++ src/optimal_control/__init__.py
@@ -0,0 +1,6 @@
+from .constraints.constraints import *
+from .costs.cost import *
+from .dynamics.dynamics import *
+from .opt_ctrl.optimal_control_base import *
+from .opt_ctrl.direct_optimal_control import *
+from .solvers.solver import *
