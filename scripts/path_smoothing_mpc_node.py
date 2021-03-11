bf1df6235750f0040223117ae119e8e0344a6056
Justin Whitaker <justinwhtkr@gmail.com>
2021-03-10 19:01:11 -0700

    Creating mpc node

--- scripts/path_smoothing_mpc_node.py
+++ scripts/path_smoothing_mpc_node.py
@@ -2,9 +2,17 @@
 
 import rospy
 
+from nav_msgs.msg import Path
+
 from path_smoothing.msg import FlatState, Trajectory
 import path_smoothing.mpc as mpc
+import tools.multi_array as multi_array
 
+
+def path_to_traj(path):
+    start_time = path.header.stamp.to_sec()
+    times = [pose_stamped.header.stamp.to_sec() for pose_stamped in path.poses]
+    poses = [pose_stamped.pose for pose_stamped in path.poses]
 
 class DiffFlatMPCNode(mpc.DiffFlatMpc):
     """
@@ -22,7 +30,7 @@ class DiffFlatMPCNode(mpc.DiffFlatMpc):
         parameters = self.get_parameters()
         super().__init__(*parameters)
 
-        rospy.Subscriber("global_path", Trajectory,
+        rospy.Subscriber("global_path", Path,
                          callback=self.global_path_cb, queue_size=10)
         # TODO: Add the iris and time-varying constraints
         # rospy.Subscriber("iris_regions", None, callback=self.iris_regions_cb,
@@ -34,8 +42,16 @@ class DiffFlatMPCNode(mpc.DiffFlatMpc):
         self.mpc_traj_pub = rospy.Publisher("mpc_traj", Trajectory,
                                             queue_size=10)
 
+        self.global_path = None
+        self.new_global_path = False
+        self.iris_regions = None
+        self.tv_constrs = None
+        self.flat_state = None
+        self.curr_control = None
+
     def global_path_cb(self, msg):
-        pass
+        self.global_path = msg
+        self.new_global_path = True
 
     def iris_regions(self, msg):
         pass
@@ -44,7 +60,20 @@ class DiffFlatMPCNode(mpc.DiffFlatMpc):
         pass
 
     def flat_state_cb(self, msg):
-        pass
+        self.flat_state = multi_array.multi_array_to_array(msg.flat_state)
+        self.flat_state = self.flat_state.reshape((msg.ndimensions,
+                                                   msg.nderivatives), order="F")
 
     def run(self):
-        pass
+        if self.global_path:
+            time = rospy.Time.now().to_sec()
+            self.curr_desired_path = self.get_desired_traj(time)
+            if self.new_global_path:
+                self.get_iris_regions(self.global_path)
+                self.get_tv_constrs(self.global_path)
+                self.new_global_path = False
+            self.mpc_output = self.mpc.step(time, self.flat_state,
+                                            self.curr_control,
+                                            self.curr_desired_traj)
+            self.curr_control = self.mpc_output[1][:,0]
+            self.mpc_traj_pub(self.mpc_to_traj(self.mpc_output))
